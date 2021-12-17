1320424bc2ea230725648175e16aaa23cae51347
JMP <jmproctor14@live.com>
2021-12-16 22:31:32 -0600

    'add item' button functionality to create cards for ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,42 @@ var addItemsBtn = document.querySelector("#add-btn");
 var recipeSummaries = ["chicken parmesan", "pancakes", "chicken strips", "mac 'n cheese"];
 
 var recipeResultsContainer = document.querySelector("#recipe-results");
-
+var ingredientContainer = document.querySelector("#ingredient-items");
+var ingredientInputs = [];
 
 
 var displayIngredients = function(ingredientInputEl) {
     var ingredientInputEl = document.getElementById("ingredient-input").value
     
-    var ingredientInputs = [];
     ingredientInputs.push(ingredientInputEl);
     console.log(ingredientInputs);
+    if (ingredientInputs) {
+        for (i = 0; i < ingredientInputs.length; i++) {
+            var cardDivEl = document.createElement("div")
+            cardDivEl.className = "card";
+            var cardContentDivEl = document.createElement("div")
+            cardContentDivEl.className = "card-content";
+            var contentDivEl = document.createElement("div")
+            contentDivEl.className = "content";
+            contentDivEl.textContent = ingredientInputs[i];
+
+            cardContentDivEl.appendChild(contentDivEl);
+            cardDivEl.appendChild(cardContentDivEl);
+            ingredientContainer.appendChild(cardDivEl);
+        }} 
+        else {
+            alert("write something.")
+        }
+
+};
+
+
+
+
+
+
+
+var displayRecipeSummaries = function() {
 
     var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0195b7fd3cb14afbaa629e0c06b3d5b8&ingredients=" + ingredientInputs;
 
@@ -25,14 +52,8 @@ var displayIngredients = function(ingredientInputEl) {
                 alert("Didn't work!")
             );
         }
-        )
-}
+        );
 
-
-
-
-
-var displayRecipeSummaries = function() {
     for (i = 0; i < recipeSummaries.length; i++) {
         var cardDivEl = document.createElement("div")
         cardDivEl.className = "card";
