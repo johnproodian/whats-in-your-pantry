b6aa33bbc9675d0b732aa6bc8906a400f51ff976
JMP <jmproctor14@live.com>
2021-12-18 06:32:21 -0600

    added click event to recipe summary cards to open the expanded recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,52 @@
 var getRecipeBtn = document.querySelector("#search-btn");
 var addItemsBtn = document.querySelector("#add-btn");
 var recipeSummaries = ["chicken parmesan", "pancakes", "chicken strips", "mac 'n cheese"];
+var placeholder = ["it worked!!"]
 
 var recipeResultsContainer = document.querySelector("#recipe-results");
 var ingredientContainer = document.querySelector("#ingredient-items");
+var expandedRecipeContainer = document.querySelector("#recipe-expanded");
 var ingredientInputs = [];
+
 
 
 var displayIngredients = function(ingredientInputEl) {
     var ingredientInputEl = document.getElementById("ingredient-input").value
-    
+    // const lastItem = ingredientInputs[ingredientInputs.length - 1]
+    console.log(ingredientInputEl);
     ingredientInputs.push(ingredientInputEl);
     console.log(ingredientInputs);
-    if (ingredientInputs) {
-        for (i = 0; i < ingredientInputs.length; i++) {
-            var cardDivEl = document.createElement("div")
-            cardDivEl.className = "card";
-            var cardContentDivEl = document.createElement("div")
-            cardContentDivEl.className = "card-content";
-            var contentDivEl = document.createElement("div")
-            contentDivEl.className = "content";
-            contentDivEl.textContent = ingredientInputs[i];
 
-            cardContentDivEl.appendChild(contentDivEl);
-            cardDivEl.appendChild(cardContentDivEl);
-            ingredientContainer.appendChild(cardDivEl);
-        }} 
-        else {
-            alert("write something.")
-        }
+    var cardDivEl = document.createElement("div")
+    cardDivEl.className = "card";
+    var cardContentDivEl = document.createElement("div")
+    cardContentDivEl.className = "card-content";
+    var contentDivEl = document.createElement("div")
+    contentDivEl.className = "content";
+    contentDivEl.textContent = ingredientInputEl;
 
+    cardContentDivEl.appendChild(contentDivEl);
+    cardDivEl.appendChild(cardContentDivEl);
+    ingredientContainer.appendChild(cardDivEl);
+    
+    
 };
 
 
+var expandRecipe = function() {
 
+    var cardDivEl = document.createElement("div")
+    cardDivEl.className = "card";
+    var cardContentDivEl = document.createElement("div")
+    cardContentDivEl.className = "card-content";
+    var contentDivEl = document.createElement("div")
+    contentDivEl.className = "content";
+    contentDivEl.textContent = placeholder;
+
+    cardContentDivEl.appendChild(contentDivEl);
+    cardDivEl.appendChild(cardContentDivEl);
+    expandedRecipeContainer.appendChild(cardDivEl);
+}
 
 
 
@@ -42,7 +55,7 @@ var displayIngredients = function(ingredientInputEl) {
 var displayRecipeSummaries = function() {
 
     var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0195b7fd3cb14afbaa629e0c06b3d5b8&ingredients=" + ingredientInputs;
-
+    
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
@@ -54,6 +67,8 @@ var displayRecipeSummaries = function() {
         }
         );
 
+    
+    
     for (i = 0; i < recipeSummaries.length; i++) {
         var cardDivEl = document.createElement("div")
         cardDivEl.className = "card";
@@ -62,11 +77,21 @@ var displayRecipeSummaries = function() {
         var contentDivEl = document.createElement("div")
         contentDivEl.className = "content";
         contentDivEl.textContent = recipeSummaries[i];
+        // var expandRecipeBtn = document.createElement("button")
+        // expandRecipeBtn.className = "button is-light m2";
+        // expandRecipeBtn.type = "button";
+        // expandRecipeBtn.id = "expand-btn";
 
         cardContentDivEl.appendChild(contentDivEl);
         cardDivEl.appendChild(cardContentDivEl);
         recipeResultsContainer.appendChild(cardDivEl);
+        // expandRecipeBtn.appendChild(contentDivEl);
+
+
+        
     }
+    
+    contentDivEl.addEventListener("click", expandRecipe);
 }
 
 getRecipeBtn.addEventListener("click", displayRecipeSummaries);
