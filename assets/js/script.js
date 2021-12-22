00404a28b0a6de91107114be877113f0dd982212
JMP <jmproctor14@live.com>
2021-12-21 21:50:21 -0600

    commented out 'bonus code'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,84 @@
-var apiKey = "ddc5bf55d47a481586f8f6a808808cad";
-var getRecipeBtn = document.querySelector("#search-btn");
-var addItemsBtn = document.querySelector("#add-btn");
-var fullRecipe = document.querySelector("#full-recipe");
+// var apiKey = "ddc5bf55d47a481586f8f6a808808cad";
+// var getRecipeBtn = document.querySelector("#search-btn");
+// var addItemsBtn = document.querySelector("#add-btn");
+// var fullRecipe = document.querySelector("#full-recipe");
 
-var recipeResultsContainer = document.querySelector("#recipe-results");
+// var recipeResultsContainer = document.querySelector("#recipe-results");
 
-var ingredientInputs = [];
+// var ingredientInputs = [];
 
-var cardDivEl = document.createElement("div")
-var cardContentDivEl = document.createElement("div")
-var contentDivEl = document.createElement("div")
+// var cardDivEl = document.createElement("div")
+// var cardContentDivEl = document.createElement("div")
+// var contentDivEl = document.createElement("div")
 
-var recipeSummaryBtns = [];
+// var recipeSummaryBtns = [];
 
-var recipeIds = [];
+// var recipeIds = [];
 
 
-// changed name from 'displayIngredients' to 'ingredientInputHandler'
-var ingredientInputHandler = function(ingredientInputEl) {
+// // changed name from 'displayIngredients' to 'ingredientInputHandler'
+// var ingredientInputHandler = function(ingredientInputEl) {
 
-    // get ingredients to the array to be used
-    // 
-    var ingredientInputEl = document.getElementById("ingredient-input").value
+//     // get ingredients to the array to be used
+//     // 
+//     var ingredientInputEl = document.getElementById("ingredient-input").value
     
-    ingredientInputs.push(ingredientInputEl);
-    console.log(ingredientInputs);
+//     ingredientInputs.push(ingredientInputEl);
+//     console.log(ingredientInputs);
 
-    // moved fetch(api) stuff to getRecipes below
-}
+//     // moved fetch(api) stuff to getRecipes below
+// }
 
-/* var getRecipes = function(displayFunction) {
-    // take ingredient array and convert it to single string
-    ingredientInputs.toString();
+// /* var getRecipes = function(displayFunction) {
+//     // take ingredient array and convert it to single string
+//     ingredientInputs.toString();
 
-    // make apiUrl that adds the single string ingredient variable
-    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0195b7fd3cb14afbaa629e0c06b3d5b8&ingredients=" + ingredientInputs;
+//     // make apiUrl that adds the single string ingredient variable
+//     var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0195b7fd3cb14afbaa629e0c06b3d5b8&ingredients=" + ingredientInputs;
     
-    // fetch api and json the response
-    fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                response.json()
-                    // call the displayRecipeSummaries w/ data array
-                    .then(function(data) {
-                        displayFunction(data);
-                    })
+//     // fetch api and json the response
+//     fetch(apiUrl)
+//         .then(function(response) {
+//             if (response.ok) {
+//                 response.json()
+//                     // call the displayRecipeSummaries w/ data array
+//                     .then(function(data) {
+//                         displayFunction(data);
+//                     })
                 
-            } else (
-                alert("Didn't work!")
-            );
-        }
-        )    
-}
-*/
+//             } else (
+//                 alert("Didn't work!")
+//             );
+//         }
+//         )    
+// }
+// */
 
-var getRecipes = function() {
-    // take ingredient array and convert it to single string
-    ingredientInputs.toString();
+// var getRecipes = function() {
+//     // take ingredient array and convert it to single string
+//     ingredientInputs.toString();
 
-    // make apiUrl that adds the single string ingredient variable
-    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=ddc5bf55d47a481586f8f6a808808cad&ingredients=" + ingredientInputs;
+//     // make apiUrl that adds the single string ingredient variable
+//     var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=ddc5bf55d47a481586f8f6a808808cad&ingredients=" + ingredientInputs;
     
-    // fetch api and json the response
-    fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                response.json()
-                    // call the displayRecipeSummaries w/ data array
-                    .then(function(data) {
-                        displayRecipeSummaries(data);
-                    })
+//     // fetch api and json the response
+//     fetch(apiUrl)
+//         .then(function(response) {
+//             if (response.ok) {
+//                 response.json()
+//                     // call the displayRecipeSummaries w/ data array
+//                     .then(function(data) {
+//                         displayRecipeSummaries(data);
+//                     })
                 
-            } else (
-                alert("Didn't work!")
-            );
-        }
-        )
-}
+//             } else (
+//                 alert("Didn't work!")
+//             );
+//         }
+//         )
+// }
+
+// Proctor Branch below
 
 var apiKey = "0195b7fd3cb14afbaa629e0c06b3d5b8";
 var getRecipeBtn = document.querySelector("#search-btn");
