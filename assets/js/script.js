558e9dd41ecab2c9f851f6b252822d637e3fdccc
Brenda Salinas <brenda.salinas2128@gmail.com>
2021-12-21 19:55:27 -0600

    adding the ability to edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// var apiKey = "0195b7fd3cb14afbaa629e0c06b3d5b8";
+// var apiKey = "c4a62d9534774af08a7a4391c080518a";
 var getRecipeBtn = document.querySelector("#search-btn");
 var addItemsBtn = document.querySelector("#add-btn");
 var recipeSummaries = ["chicken parmesan", "pancakes", "chicken strips", "mac 'n cheese"];
@@ -6,31 +6,30 @@ var recipeSummaries = ["chicken parmesan", "pancakes", "chicken strips", "mac 'n
 var recipeResultsContainer = document.querySelector("#recipe-results");
 
 
-
+var ingredientInputs = [];
 var displayIngredients = function(ingredientInputEl) {
-    var ingredientInputEl = document.getElementById("ingredient-input").value
-    
-    var ingredientInputs = [];
-    ingredientInputs.push(ingredientInputEl);
+    var ingredientInputEl = document.getElementById("ingredient-input")
+
+    ingredientInputs.push(ingredientInputEl.value);
+    ingredientInputEl.value = "";
     console.log(ingredientInputs);
 
-    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0195b7fd3cb14afbaa629e0c06b3d5b8&ingredients=" + ingredientInputs;
+    var apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=c4a62d9534774af08a7a4391c080518a&ingredients=" + ingredientInputs;
 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json()
-                
+                // displaying function 
+                .then(function(data) {
+                    displayFunction(data);
+                })
             } else (
                 alert("Didn't work!")
             );
         }
         )
 }
-
-
-
-
 
 var displayRecipeSummaries = function() {
     for (i = 0; i < recipeSummaries.length; i++) {
